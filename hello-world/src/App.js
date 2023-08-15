8d8abb51fd9cdf21e7d4743e587ab1b758f1ca71
JangSoyun <soyoon5328@gmail.com>
2023-08-15 18:19:32 +0900

    init: create-react-app

---
+++ hello-world/src/App.js
@@ -0,0 +1,11 @@
+import Homepage from "./components/homePage/Homepage";
+function App() {
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/components/common/MyHeader.jsx
@@ -0,0 +1,12 @@
+function MyHeader() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h1>난 헤더입니다</h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default MyHeader;
+  
